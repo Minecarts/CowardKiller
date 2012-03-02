3475634f0895b2422d288086a3651cd8668a9d5e
Stephen <stephen@jazer.com>
2012-03-01 19:59:55 -0500

    Updated for new bukkit event system (1.1-R6)

--- src/com/minecarts/cowardkiller/CowardKiller.java
+++ src/com/minecarts/cowardkiller/CowardKiller.java
@@ -6,14 +6,11 @@ import java.text.MessageFormat;
 
 import org.bukkit.configuration.file.FileConfiguration;
 
-import org.bukkit.event.Event.Type;
-import org.bukkit.event.Event.Priority;
-
-import org.bukkit.event.entity.EntityListener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-
-import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 
@@ -29,18 +26,65 @@ import org.bukkit.entity.Tameable;
 import org.bukkit.entity.Projectile;
 
 
-public class CowardKiller extends org.bukkit.plugin.java.JavaPlugin {
-    private static final Logger logger = Logger.getLogger("com.minecarts.cowardkiller"); 
+public class CowardKiller extends org.bukkit.plugin.java.JavaPlugin implements Listener {
+    private static Logger logger;
     
     protected FileConfiguration config;
     protected boolean debug;
     protected int combatWindow;
     
     protected WeakHashMap<Player, DatedEvent> lastDamageEvents = new WeakHashMap<Player, DatedEvent>();
-    
-    
+
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onEntityDamage(EntityDamageEvent event) {
+        if(event.isCancelled()) return;
+
+        if(event instanceof EntityDamageByEntityEvent) {
+            Player defender = getParentPlayer(event.getEntity());
+            Player attacker = getParentPlayer(((EntityDamageByEntityEvent) event).getDamager());
+
+            if(defender != null && attacker != null && !attacker.equals(defender)) {
+                debug("{0} attacked {1}, storing current date for each", attacker.getName(), defender.getName());
+
+                DatedEvent now = new DatedEvent(event);
+                lastDamageEvents.put(attacker, now);
+                lastDamageEvents.put(defender, now);
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        DatedEvent lastDamage = lastDamageEvents.get(player);
+
+        if(lastDamage != null) {
+            float secondsSinceLastDamage = lastDamage.elapsed() / 1000;
+
+            if(combatWindow > secondsSinceLastDamage) {
+                int penalty = (int) Math.round(player.getMaxHealth() * (1 - (secondsSinceLastDamage / combatWindow)));
+
+                log("Penalizing {0} with {1} damage for logging out while in PVP combat", player.getName(), penalty);
+
+                if(lastDamage.getEvent() instanceof EntityDamageByEntityEvent) {
+                    EntityDamageByEntityEvent damageEvent = (EntityDamageByEntityEvent) lastDamage.getEvent();
+                    log("Last EntityDamageByEntityEvent: {0} inflicted {1} damage on {2} {3} seconds ago", damageEvent.getDamager(), damageEvent.getDamage(), damageEvent.getEntity(), lastDamage.elapsed() / 1000);
+                }
+
+                PlayerCombatPenaltyEvent penaltyEvent = new PlayerCombatPenaltyEvent(player, penalty, event);
+                getServer().getPluginManager().callEvent(penaltyEvent);
+
+                if(!penaltyEvent.isCancelled()) {
+                    player.damage(penaltyEvent.getDamage());
+                }
+            }
+        }
+    }
+
     public void onEnable() {
         reloadConfig();
+        logger = getLogger();
         
         // reload config command
         getCommand("cowardkiller").setExecutor(new CommandExecutor() {
@@ -57,61 +101,7 @@ public class CowardKiller extends org.bukkit.plugin.java.JavaPlugin {
                 return false;
             }
         });
-        
-        
-        // record last PVP damage event
-        getServer().getPluginManager().registerEvent(Type.ENTITY_DAMAGE, new EntityListener() {
-            @Override
-            public void onEntityDamage(EntityDamageEvent event) {
-                if(event.isCancelled()) return;
-                
-                if(event instanceof EntityDamageByEntityEvent) {
-                    Player defender = getParentPlayer(event.getEntity());
-                    Player attacker = getParentPlayer(((EntityDamageByEntityEvent) event).getDamager());
-                    
-                    if(defender != null && attacker != null && !attacker.equals(defender)) {
-                        debug("{0} attacked {1}, storing current date for each", attacker.getName(), defender.getName());
-                        
-                        DatedEvent now = new DatedEvent(event);
-                        lastDamageEvents.put(attacker, now);
-                        lastDamageEvents.put(defender, now);
-                    }
-                }
-            }
-        }, Priority.Monitor, this);
-        
-        // penalize combat logging
-        getServer().getPluginManager().registerEvent(Type.PLAYER_QUIT, new PlayerListener() {
-            @Override
-            public void onPlayerQuit(PlayerQuitEvent event) {
-                Player player = event.getPlayer();
-                DatedEvent lastDamage = lastDamageEvents.get(player);
-                
-                if(lastDamage != null) {
-                    float secondsSinceLastDamage = lastDamage.elapsed() / 1000;
-                    
-                    if(combatWindow > secondsSinceLastDamage) {
-                        int penalty = (int) Math.round(player.getMaxHealth() * (1 - (secondsSinceLastDamage / combatWindow)));
-                        
-                        log("Penalizing {0} with {1} damage for logging out while in PVP combat", player.getName(), penalty);
-                        
-                        if(lastDamage.getEvent() instanceof EntityDamageByEntityEvent) {
-                            EntityDamageByEntityEvent damageEvent = (EntityDamageByEntityEvent) lastDamage.getEvent();
-                            log("Last EntityDamageByEntityEvent: {0} inflicted {1} damage on {2} {3} seconds ago", damageEvent.getDamager(), damageEvent.getDamage(), damageEvent.getEntity(), lastDamage.elapsed() / 1000);
-                        }
-                        
-                        PlayerCombatPenaltyEvent penaltyEvent = new PlayerCombatPenaltyEvent(player, penalty, event);
-                        getServer().getPluginManager().callEvent(penaltyEvent);
-                        
-                        if(!penaltyEvent.isCancelled()) {
-                            player.damage(penaltyEvent.getDamage());
-                        }
-                    }
-                }
-            }
-        }, Priority.Monitor, this);
-        
-        
+
         log("Version {0} enabled.", getDescription().getVersion());
     }
     
